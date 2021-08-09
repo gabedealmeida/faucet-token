75cfc0e1897e29f303216cf7934dfa2ba8c002d0
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-08-09 17:39:23 -0400

    Faucet token working initial implementation

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import Faucet from './components/Faucet.js'
+import TokenSend from './components/TokenSend.js'
+
+function App() {
+
+  const Token = FCTToken;
+
+  return (
+    <div className="App">
+    <Container>
+    <Row className="justify-content-md-center">
+      <Col>
+      <Faucet tokenContract={Token}/>
+      </Col>
+      <Col>
+      <TokenSend tokenContract={Token}/>
+      </Col>
+    </Row>
+    </Container>
+    </div>
+  );
+}
+
+export default App;
